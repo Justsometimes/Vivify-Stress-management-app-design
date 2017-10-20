b0574cb786cce7a663d1e36e12be9f03887552fc
WasBever <32698849+WasBever@users.noreply.github.com>
2017-10-20 11:51:02 +0200

    Added buttoneffects

--- app/src/main/java/com/example/user/vivifystressmanagementappdesign/MainActivity.java
+++ app/src/main/java/com/example/user/vivifystressmanagementappdesign/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.user.vivifystressmanagementappdesign;
+
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,34 +16,78 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+
+    public static void buttonEffect(View button) {
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonEffect(findViewById(R.id.button7));
+            }
+        });
+        final Button button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonEffect(findViewById(R.id.button8));
+            }
+        });
+        final Button button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonEffect(findViewById(R.id.button9));
+            }
+        });
+        final Button button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonEffect(findViewById(R.id.button10));
+            }
+        });
+        final Button button11 = (Button) findViewById(R.id.button11);
+        button11.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonEffect(findViewById(R.id.button11));
+            }
+        });
+        final Button button12 = (Button) findViewById(R.id.button12);
+        button12.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonEffect(findViewById(R.id.button12));
+            }
+        });
+        final Button button13 = (Button) findViewById(R.id.button13);
+        button13.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonEffect(findViewById(R.id.button13));
+            }
+        });
     }
 
-    //public static void buttonEffect(View button) {
-      //  button.setOnTouchListener(new View.OnTouchListener() {
-
-      //      public boolean onTouch(View v, MotionEvent event) {
-        //        switch (event.getAction()) {
-          //          case MotionEvent.ACTION_DOWN: {
-            //            v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
-              //          v.invalidate();
-                //        break;
-                  //  }
-                    //case MotionEvent.ACTION_UP: {
-                      //  v.getBackground().clearColorFilter();
-                        //v.invalidate();
-                        //break;
-                    //}
-                //}
-                //return false;
-        //    }
-       // });
-    //}
     // A native method that is implemented by the 'native-lib' native library,
     // which is packaged with this application.
-   public native String stringFromJNI();
+    public native String stringFromJNI();
 }
